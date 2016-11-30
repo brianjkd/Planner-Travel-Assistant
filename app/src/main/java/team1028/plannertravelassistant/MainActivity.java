b8a7db724e799b97f785f638b8a85892254a5809
Brianjkd <brianjkd@gmail.com>
2016-11-30 17:24:00 -0500

    Added Calendar

--- app/src/main/java/team1028/plannertravelassistant/MainActivity.java
+++ app/src/main/java/team1028/plannertravelassistant/MainActivity.java
@@ -8,30 +8,34 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     // Location Permissions variables
-    private static final int ACCESS_FINE_LOCATION = 201;
-    private static String[] PERMISSIONS_LOCATION = {
-            Manifest.permission.ACCESS_FINE_LOCATION
+    private static final int REQUEST_CODE = 201;
+    //private static final int READ_CALENDAR = 202;
+    private static String[] PERMISSIONS_ARRAY = {
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_CALENDAR
     };
 
     // permission verification method.
     public static void verifyLocationPermissions(Activity activity) {
         // Check if we have read or write permission
         int fineLocationPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION);
-        if (fineLocationPermission != PackageManager.PERMISSION_GRANTED) {
+        int readCalendarPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_CALENDAR);
+        if (fineLocationPermission != PackageManager.PERMISSION_GRANTED
+                && readCalendarPermission != PackageManager.PERMISSION_GRANTED) {
             // We don't have permission so prompt the user
             ActivityCompat.requestPermissions(
                     activity,
-                    PERMISSIONS_LOCATION,
-                    ACCESS_FINE_LOCATION
+                    PERMISSIONS_ARRAY,
+                    REQUEST_CODE
             );
         }
     }
@@ -84,5 +88,24 @@ public class MainActivity extends AppCompatActivity {
         LocalBroadcastManager.getInstance(this).registerReceiver(
                 mMessageReceiver, new IntentFilter("newLocation"));
     }
+
+
+
+    // calendar stuff
+    // Projection array. Creating indices for this array instead of doing
+// dynamic lookups improves performance.
+    public static final String[] EVENT_PROJECTION = new String[] {
+            CalendarContract.Calendars._ID,                           // 0
+            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+    };
+
+    // The indices for the projection array above.
+    private static final int PROJECTION_ID_INDEX = 0;
+    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+
 
 }
--- app/src/main/java/team1028/plannertravelassistant/MyServiceIntent.java
+++ app/src/main/java/team1028/plannertravelassistant/MyServiceIntent.java
@@ -4,6 +4,12 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import java.util.List;
+
+import me.everything.providers.android.calendar.Calendar;
+import me.everything.providers.android.calendar.CalendarProvider;
+import me.everything.providers.android.calendar.Event;
+
 public class MyServiceIntent extends IntentService {
 
     public static final String TAG = "MyServiceIntent";
@@ -20,7 +26,26 @@ public class MyServiceIntent extends IntentService {
     @Override
     protected void onHandleIntent(Intent workIntent) { // do stuff when alarm starts this intent
         Log.d(TAG, "onHandleIntent: Do stuff");
+
+        //CalendarStuff cs = new CalendarStuff();
+       // cs.readCalendarEvent(this);
+/*
+        CalendarContentResolver crs = new CalendarContentResolver(this);
+        crs.getCalendars();*/
+/*
+        CalendarService cs = new CalendarService();
+        cs.readCalendar(this, 7, 0);*/
+
+        CalendarProvider calendarProvider = new CalendarProvider(this);
+        List<Calendar> calendars = calendarProvider.getCalendars().getList();
+
+
+        for (Calendar c : calendars){
+            List<Event> events = calendarProvider.getEvents(c.id).getList();
+                for (Event e : events){
+                    Log.d(TAG, "event name " +   e.title );
+                }
+        }
+        
     }
-
-
 }
