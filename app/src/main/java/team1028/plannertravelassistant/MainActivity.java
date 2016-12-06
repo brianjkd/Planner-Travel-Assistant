6ced64d37f8a756d72915a9aee89f41148e8ee71
Brianjkd <brianjkd@gmail.com>
2016-12-05 20:02:30 -0500

    added calendar and map view

--- app/src/main/java/team1028/plannertravelassistant/MainActivity.java
+++ app/src/main/java/team1028/plannertravelassistant/MainActivity.java
@@ -8,35 +8,61 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import java.util.ArrayList;
+
+import static team1028.plannertravelassistant.R.id.mapView;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 	public static final String TAG = "MainActivity"; // TODO add description
 
 	Location curLocation; // Store current location
 
     // Location Permissions variables
-    private static final int ACCESS_FINE_LOCATION = 201;
-    private static String[] PERMISSIONS_LOCATION = {
-            Manifest.permission.ACCESS_FINE_LOCATION
+    private static final int REQUEST_CODE = 201;
+    //private static final int READ_CALENDAR = 202;
+    private static String[] PERMISSIONS_ARRAY = {
+            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_CALENDAR
     };
+    ArrayAdapter<String> adapter;
+    ArrayList<String> listItems=new ArrayList<String>();
+    private ListView list;
+
+    GoogleMap googleMap;
+
+    // this comment is a test for the project build
 
     // permission verification method.
     public static void verifyLocationPermissions(Activity activity) {
         // Check if we have read or write permission
-        int fineLocationPermission = ActivityCompat.checkSelfPermission(activity,
-                Manifest.permission.ACCESS_FINE_LOCATION);
 
-        if (fineLocationPermission != PackageManager.PERMISSION_GRANTED) {
+        int fineLocationPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION);
+        int readCalendarPermission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.READ_CALENDAR);
+        if (fineLocationPermission != PackageManager.PERMISSION_GRANTED
+                && readCalendarPermission != PackageManager.PERMISSION_GRANTED) {
+
             // We don't have permission so prompt the user
             ActivityCompat.requestPermissions(
                     activity,
-                    PERMISSIONS_LOCATION,
-                    ACCESS_FINE_LOCATION
+                    PERMISSIONS_ARRAY,
+                    REQUEST_CODE
             );
         }
     }
@@ -67,12 +93,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent i = new Intent(this, ForegroundService.class);
 
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(mapView);
+        mapFragment.getMapAsync(this);
+
         verifyLocationPermissions(this);
 
+        // configure architecture so that this takes the event list from MyServiceIntent
+        listItems.add("test 1");
+        listItems.add("test 2");
+        listItems.add("test 1");
+        listItems.add("test 3");
+        listItems.add("test 1");
+        listItems.add("test 2");
+        listItems.add("test 1");
+        listItems.add("test 3");
+
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        list = (ListView) findViewById(R.id.eventList);
+        list.setAdapter(adapter);
         this.startService(i);
     }
 
-    public void onPause() { // TODO save instance state?
+    public void onPause(){
         super.onPause();
         // unregister message Receiver when app is not focused
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
@@ -83,6 +128,34 @@ public class MainActivity extends AppCompatActivity {
         // listen for intents with the filter "newLocation"
         LocalBroadcastManager.getInstance(this).registerReceiver(
                 mMessageReceiver, new IntentFilter("newLocation"));
+    }
+
+
+
+    // calendar stuff
+    // Projection array. Creating indices for this array instead of doing
+// dynamic lookups improves performance.
+    public static final String[] EVENT_PROJECTION = new String[] {
+            CalendarContract.Calendars._ID,                           // 0
+            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+    };
+
+    // The indices for the projection array above.
+    private static final int PROJECTION_ID_INDEX = 0;
+    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        /**
+         * This is where we can add markers or lines, add listeners or move the camera. In this case,
+         * we add a marker to Worcester, MA (WPI's location).
+         */
+        map.addMarker(new MarkerOptions().position(new LatLng(42.2722, -71.8038)).title("Marker"));
     }
 
 }
--- app/src/main/java/team1028/plannertravelassistant/MyServiceIntent.java
+++ app/src/main/java/team1028/plannertravelassistant/MyServiceIntent.java
@@ -4,6 +4,14 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import me.everything.providers.android.calendar.Calendar;
+import me.everything.providers.android.calendar.CalendarProvider;
+import me.everything.providers.android.calendar.Event;
+
 public class MyServiceIntent extends IntentService {
 
     public static final String TAG = "MyServiceIntent";
@@ -19,5 +27,48 @@ public class MyServiceIntent extends IntentService {
     @Override
     protected void onHandleIntent(Intent workIntent) { // do stuff when alarm starts this intent
         Log.d(TAG, "onHandleIntent: Do stuff");
+
+
+        //CalendarStuff cs = new CalendarStuff();
+       // cs.readCalendarEvent(this);
+/*
+        CalendarContentResolver crs = new CalendarContentResolver(this);
+        crs.getCalendars();*/
+/*
+        CalendarService cs = new CalendarService();
+        cs.readCalendar(this, 7, 0);*/
+
+        CalendarProvider calendarProvider = new CalendarProvider(this);
+        List<Calendar> calendars = calendarProvider.getCalendars().getList();
+
+        getFilteredList(calendarProvider, calendars);
+    }
+
+    protected void getFilteredList(CalendarProvider calendarProvider, List<Calendar> calendars)
+    {
+        for (Calendar c : calendars){
+            List<Event> events = calendarProvider.getEvents(c.id).getList();
+            String location = "";
+            long currTime = System.currentTimeMillis();
+            long msInTwelve = currTime + (60*60*12*1000);
+            ArrayList<Event> filtered = new ArrayList<Event>();
+            for (Event e : events){
+                location = e.eventLocation;
+                if (e.eventLocation != null && !e.eventLocation.isEmpty())
+                {
+                    Log.d(TAG, "event name " +   e.title );
+                    Log.d(TAG, "event location " + e.eventLocation);
+                    System.out.println("current time " + currTime);
+                    System.out.println("twelve hours from now" + msInTwelve);
+                    if (e.dTStart > currTime && e.dTStart < msInTwelve)
+                    {
+                        filtered.add(e);
+                        System.out.println(location.length());
+                        System.out.println("test "+ location);
+                    }
+                }
+            }
+        }
+
     }
 }
